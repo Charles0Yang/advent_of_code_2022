eaf4d30245a74d541400e39db45c4a44d689c2d6
Charles Yang <cyang@dhcp-10-249-173-164.eduroam.wireless.private.cam.ac.uk>
2022-12-05 22:01:51 +0000

    Day 4 completed

---
+++ day_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("input.txt");  
+
+        // Part 1
+
+        int total = 0;
+        int total_two = 0;
+
+        foreach(string line in lines){
+            String[] delimiters = {"-", ","};
+            String[] arr = line.Split(delimiters, StringSplitOptions.None);
+            int[] a = Array.ConvertAll(arr, s => int.Parse(s));
+            total += ((a[0] <= a[2] && a[1] >= a[3]) || (a[0] >= a[2] && a[1] <= a[3])) ? 1 : 0;
+            total_two += ((a[0] <= a[2] && a[1] >= a[2]) || (a[2] <= a[0] && a[3] >= a[0])) ? 1 : 0;
+        }
+        Console.WriteLine(total);
+        Console.WriteLine(total_two);
+
+    }
+}
